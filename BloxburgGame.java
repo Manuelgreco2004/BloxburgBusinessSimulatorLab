13f1ddb9321711da0e6540bcd042bd842c402973
Manuelgreco2004 <Greco.M216053@stu.sanjac.edu>
2026-09-09 20:20:12 +0000

    Business.java

--- BloxburgGame.java
+++ BloxburgGame.java
@@ -2,6 +2,12 @@ public class BloxburgGame
 {
     public static void main (String[] args)
     {
+        Business pizzaPlace = new Business("Pizza Palace", "Alex", 5000);
+        Business.Employee pizzaEmployee = pizzaPlace.new Employee("John", "Chef", 15);
+ 
+        pizzaPlace.payTaxes();
+    }
+}
         
     }
 }
